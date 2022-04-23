a5590b81483f307133066123cfea86635b1d3ee4
Daniel Barnes <dabarnes2b@gmail.com>
2022-04-24 06:26:24 +0900

    Add debug logging (#20)
    * add debug logging
    
    * npm run build
    
    * one try block
    
    * auto-closer patch
    
    * auto-closer patch
    
    * prettier printing

--- dist/index.js
+++ dist/index.js
@@ -8573,18 +8573,26 @@ const listToArray = (str) => {
 
     const rtrue = { required: true };
     const token = Core.getInput('token', rtrue);
+    Core.debug(`Using token: ${token}`);
 
     // required but defaults not handled by action.yml
     const repoContext = Github.context.repo;
     const owner = Core.getInput('owner') || repoContext.owner;
     const repo = Core.getInput('repo') || repoContext.repo;
     const title = Core.getInput('title', rtrue);
+    Core.debug(`Using owner: ${owner}`);
+    Core.debug(`Using repo: ${repo}`);
+    Core.debug(`Using title: ${title}`);
 
     // optional
     const body = Core.getInput('body');
     const milestone = Core.getInput('milestone');
     const labels = Core.getInput('labels');
     const assignees = Core.getInput('assignees');
+    Core.debug(`Using body: """${body}"""`);
+    Core.debug(`Using milestone: ${milestone}`);
+    Core.debug(`Using labels: ${labels}`);
+    Core.debug(`Using assignees: ${assignees}`);
 
     const octokit = Github.getOctokit(token);
     const opts = Object.fromEntries(Object.entries({
@@ -8596,13 +8604,14 @@ const listToArray = (str) => {
       labels: labels ? listToArray(labels) : null,
       assignees: assignees ? listToArray(assignees) : null
     }).filter(([_, v]) => v != null));
-
+    Core.debug(`Object for new issue: """${JSON.stringify(opts, null, 2)}"""`)
     // https://docs.github.com/en/rest/reference/issues#create-an-issue
     const newIssue = await octokit.rest.issues.create(opts);
-    console.log('Created:', newIssue.data.html_url);
+    Core.info(`Created: ${newIssue.data.html_url}`)
   } catch (err) {
 
-    Core.setFailed(err.message);
+    Core.error(err);
+    Core.setFailed('Request to create new issue failed');
   }
 })();
 
